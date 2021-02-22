4af171e007b3584925990395b1efa454e0f7b405
Marcos Rodrigues <59182008+zMarcoos@users.noreply.github.com>
2021-02-22 16:22:54 -0300

    Update WorldUtility.java

--- src/main/java/marcos/devsoftware/skywars/utility/WorldUtility.java
+++ src/main/java/marcos/devsoftware/skywars/utility/WorldUtility.java
@@ -29,25 +29,29 @@ public class WorldUtility {
 
     @SneakyThrows
     private void copyWorld(File src, File dest) {
-        if (src.getName().equalsIgnoreCase("uid.dat"))
-            return;
+        if (src.getName().equalsIgnoreCase("uid.dat")) return;
         if (src.isDirectory()) {
-            if (!dest.exists())
+            if (!dest.exists()) {
                 dest.mkdir();
+            }
+            
             String[] files = src.list();
-            if (files != null)
+            if (files != null) {
                 for (String file : files) {
                     File srcFile = new File(src, file);
                     File destFile = new File(dest, file);
                     copyWorld(srcFile, destFile);
                 }
+            }
         } else {
             InputStream in = new FileInputStream(src);
             OutputStream out = new FileOutputStream(dest);
             byte[] buffer = new byte[1024];
             int length;
-            while ((length = in.read(buffer)) > 0)
+            while ((length = in.read(buffer)) > 0) {
                 out.write(buffer, 0, length);
+            }
+            
             in.close();
             out.close();
         }
@@ -70,34 +74,14 @@ public class WorldUtility {
 
     public void deleteWorld(World world) {
         if (world == null) return;
+        
         Bukkit.getServer().unloadWorld(world, false);
         File deleteFolder = world.getWorldFolder();
+        
         worldDelete(deleteFolder);
     }
 
     public String nameGenerator(String worldName) {
         return worldName + "-" + UUID.randomUUID();
-    }
-
-    public Map<Location, String> getChestFromWorld(Location locationOne, Location locationTwo) {
-        Map<Location, String> chestList = new HashMap<>();
-        int minX = Math.min(locationOne.getBlockX(), locationTwo.getBlockX());
-        int minY = Math.min(locationOne.getBlockY(), locationTwo.getBlockY());
-        int minZ = Math.min(locationOne.getBlockZ(), locationTwo.getBlockZ());
-        int maxX = Math.max(locationOne.getBlockX(), locationTwo.getBlockX());
-        int maxY = Math.max(locationOne.getBlockY(), locationTwo.getBlockY());
-        int maxZ = Math.max(locationOne.getBlockZ(), locationTwo.getBlockZ());
-
-        for (int x = minX; x < maxX; x++) {
-            for (int y = minY; y < maxY; y++) {
-                for (int z = minZ; z < maxZ; z++) {
-                    Location location = new Location(locationOne.getWorld(), x, y, z);
-                    if (location.getBlock().getType() == Material.CHEST)
-                        chestList.put(location, "chest0");
-                }
-            }
-        }
-
-        return chestList;
     }
 }
